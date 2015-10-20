2b6595f2a762d1525f9801f70556533d45a9c236
Hans Polders <hans.polders@gmail.com>
2015-10-20 10:11:53 +0200

    Wrote some tests for abstract classes

---
+++ UnitTests.Iniect.io/AbstractTests/AbstractTests.cs
@@ -0,0 +1,37 @@
+using Iniect.io;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.Iniect.io.AbstractTests
+{
+    [TestClass]
+    public class AbstractTests
+    {
+        [TestMethod]
+        public void BindInterfaceToAbstractClass_Exception()
+        {
+            var f = new Factory();
+
+            f.Bind<ITestInterface, AbstractTestClass>();
+        }
+
+        [TestMethod]
+        public void BindInterfaceToClass()
+        {
+            var f = new Factory();
+
+            f.Bind<ITestInterface, TestClass>();
+
+            Assert.IsNotNull(f.CreateNew<ITestInterface>());
+        }
+
+        [TestMethod]
+        public void BindAbstractClassToClass()
+        {
+            var f = new Factory();
+
+            f.Bind<AbstractTestClass, TestClass>();
+
+            Assert.IsNotNull(f.CreateNew<AbstractTestClass>());
+        }
+    }
+}
